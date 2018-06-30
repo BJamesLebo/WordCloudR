0471653c164f7af240a5a39cfd0e2196b86375bc
Ben <lebo247@verizon.net>
2018-06-30 15:14:29 -0400

    Update KawhiLeonardTracker.R

--- KawhiLeonardTracker.R
+++ KawhiLeonardTracker.R
@@ -4,10 +4,10 @@ library(httr)
 library(RCurl)
 
 #Setting up Twitter authorization to use their API
-consumer_key <- "vm9t6zkw3ONSML5jondFngbIY"
-consumer_secret <- "ArmuCR98C4YjPvZRRbeWDVG5mlXhynpjuy655PO2SgB37a4FmT"
-access_token <- "872276736414801920-3Vd1tuCxlYAfufsCNU51K2scjUxT6pz"
-access_secret <- "KoGP5EWYfoacutwZmpBf6AnhDTOYsJBDj75WpleMYANHN"
+consumer_key <- "abcdefg12345678"
+consumer_secret <- "abcdefg12345678"
+access_token <- "abcdefg12345678-these.are.unique.to.you"
+access_secret <- "abcdefg12345678"
 
 #mining tweets
 setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
@@ -15,17 +15,20 @@ tw = twitteR::searchTwitter('#Kawhi + #Kawhi Leonard', n = 500,
                             since = "2018-06-29", retryOnRateLimit = 450)
 d = twitteR::twListToDF(tw)
 
-#mining tweets a different way
+#mining tweets a different way (prefer this way, used it for the wordcloud)
 Kawhi_tweets <- searchTwitter("Kawhi",n=1000,lang="en")
 
 install.packages("tm")
+library(tm)
 install.packages("wordcloud")
+library(wordcloud)
 
 #converting the list of tweets to character type
 Kawhi_text <- sapply(Kawhi_tweets, function(x) x$getText())
 
 Kawhi_corpus <- Corpus(VectorSource(Kawhi_text))
 
+#cleaning up the word data                     
 #remove punctuation
 Kawhi_clean <- tm_map(Kawhi_corpus, removePunctuation)
 #convert everything to lower case
@@ -42,6 +45,7 @@ Kawhi_clean <- tm_map(Kawhi_clean, removeWords, c("kawhi","leonard"))
 #creating the word cloud
 wordcloud(Kawhi_clean)
 
+#wordcloud function has many different parameters, play around with it, here's a few options below
 wordcloud(Kawhi_clean, random.order = F, max.words=40)
 wordcloud(Kawhi_clean, random.order=F,
           max.words=22,col=c("purple","yellow"),scale=c(2,0.5))
